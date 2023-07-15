54b1f82aa4dcb8dcd80ddc293ae7c0329dd7998b
splashling <splashling1789@gmail.com>
2023-07-15 12:24:21 +0200

    Añadida la dependencia clearscreen e implementada la opción 4 del menú principal.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 mod connection_manager;
 
+use clearscreen::clear;
 use connection_manager::connection_manager::connect;
-use std::env;
-use std::env::VarError;
-use std::io::{BufRead, stdin};
+use std::io::{BufRead, Read, stdin};
 use std::ops::{Add, Deref};
-use dotenv::{Error, from_path};
-use mysql::{Pool, PooledConn};
+use mysql::{params, PooledConn};
 use mysql::prelude::Queryable;
 
-const DEBUG:bool = true;
-const TITLE:&str = "LA BODEGA ALBERO";
-const MENU_SIZE:u16 = 55;
 
+///Título del programa
+const TITLE:&str = "LA BODEGA ALBERO";
+///Tamaño del título
+const MENU_SIZE:u16 = 55;
 
 #[doc= "Imprime el título determinado por la constante TITLE inicial con una anchura determinada por la constante MENU_SIZE"]
 macro_rules! print_title {
@@ -48,7 +47,7 @@ struct Existencia {
 }
 
 
-
+//TODO: reorganizar funciones read, insert, get y print en un módulo aparte
 fn read_categories(connection: &mut PooledConn) -> Vec<Categoria> {
     //!Lee la tabla categorias y la devuelve como un vector de estructuras "Categoria"
     return connection.query_map("SELECT id, nombre, descripcion FROM categorias;", |(id, nombre, descripcion)| {
@@ -112,8 +111,22 @@ fn print_objects(objects:Vec<Objeto>) {
     }
 }
 
-fn menu() {
+fn insert_category(conn: &mut PooledConn, name: String, desc: String) -> Result<(), mysql::Error>{
+    //!Inserta un registro de la tabla categorías dado nombre y descripción.
+    return conn.exec_drop("INSERT INTO categorias (nombre, descripcion), VALUES (:nombre, :descripcion)", params! {
+        "name" => name,
+        "descripcion" => desc,
+    });
+}
+
+fn delete_category(conn: &mut PooledConn, id: i32) -> Result<(), mysql::Error> {
+    //!Elimina un registro de la tabla categorías dado su id.
+    return conn.exec_drop("DELETE FROM categorias WHERE id=:id;", params!("id" => id));
+}
+
+fn menu(connection: &mut PooledConn, option: &mut String) {
     //!Ejecuta el menú de selección de operaciones.
+    print_title!();
     println!("1. Consultar existencias");
     println!("2. Añadir o retirar existencias");
     println!("3. Transladar existencias");
@@ -121,19 +134,84 @@ fn menu() {
     println!("5. Editar objetos");
     println!("6. Sobre el programa");
     println!("\n¿Qué deseas hacer?");
-    let mut option = String::new();
-    stdin().read_line(&mut option);
+    *option = String::from("");
+    stdin().read_line( option);
+    //TODO: opción 5
+    //TODO: opción 3
+    //TODO: opción 1
+    //TODO: opción 2
+    match option.trim() {
+        "4" => {
+            *option = String::from("");
+            clear();
+            print_categories(read_categories(connection));
+            println!("Qué desea realizar?");
+            println!("1. Agregar una categoría");
+            println!("2. Eliminar una categoría");
+            println!("{:?}", option);
+            stdin().read_line(option);
+            match option.trim() {
+                "1" => {
+                    *option = String::from("");
+                    let mut nombre = String::new();
+                    let mut desc = String::new();
+                    println!("Nombre de la nueva categoría: ");
+                    stdin().read_line(&mut nombre);
+                    println!("\nDescripción de la nueva categoría: ");
+                    stdin().read_line(&mut desc);
+                    match insert_category(connection, nombre, desc) {
+                        Ok(()) => {
+                            println!("La categoría se creó satisfactoriamente");
+                        }
+                        Err(e) => {
+                            println!("Ocurrió un error al crear la categoría: {}", e);
+                        }
+                    }
+                }
+                "2"  => {
+                    *option = String::from("");
+                    let mut inp = String::new();
+                    println!("Introduce el ID de la categoría a eliminar: ");
 
+                    stdin().read_line(&mut inp);
+                    match inp.trim().parse::<i32>() {
+                        Ok(id) => {
+                            match delete_category(connection, id) {
+                                Ok(()) => {
+                                    println!("La categoría con id: {}, fue eliminada satisfactoriamente", id);
+                                }
+                                Err(e) => {
+                                    println!("Ocurrió un error al eliminar la categoría: {}", e);
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            println!("Ocurrió un error con los datos que ha proporcionado. ¿Ha puesto algo que no sea un número? {}", e);
+                        }
+                    }
+                }
+                _ => {
+                    println!("No ha seleccionado ninguna opción. Volviendo al menú");
+                }
+            }
+        }
+        _ => {
+            println!("No ha seleccionado ninguna opción. Volviendo al menú");
+        }
+    }
 }
 
 
 fn main() {
     //!Se conecta a la base de datos, y ejecuta el menú para comenzar a realizar operaciones sobre ella.
+    println!("Conectando a la base de datos...");
+
     match connect(connection_manager::connection_manager::get_envs()) {
         Ok(c) => {
+            clear();
             let mut connection =c;
-            print_categories(read_categories(&mut connection));
-            menu();
+            let mut option = String::new();
+            loop {menu(&mut connection, &mut option)};
         }
         Err(e) => {
             println!("Ocurrió un error al conectarse a la base de datos: {}", e)
